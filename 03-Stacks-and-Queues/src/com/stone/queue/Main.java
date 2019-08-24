8053f80be73a717325bae3aae2ad7ade8367dd77
ltjs1024 <78282271@qq.com>
2019-08-24 14:12:07 +0800

    Queue

---
+++ 03-Stacks-and-Queues/src/com/stone/queue/Main.java
@@ -0,0 +1,20 @@
+package com.stone.queue;
+
+/**
+ * 测试类
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Queue<Integer> queue = new ArrayQueue<>();
+        for (int i = 0; i < 10; i++) {
+            queue.enqueue(i);
+            System.out.println(queue);
+
+            if (i % 3 == 2) {
+                queue.dequeue();
+                System.out.println(queue);
+            }
+        }
+    }
+}
