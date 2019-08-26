97100e6003c1a8ff8d43c4be91021500898b1ca7
ltjs1024 <78282271@qq.com>
2019-08-26 14:38:59 +0800

    use dummyHead

--- 04-LinkedList/src/LinkedList.java
+++ 04-LinkedList/src/LinkedList.java
@@ -26,11 +26,11 @@ public class LinkedList<E> {
         }
     }
 
-    private Node head;
+    private Node dummyHead;
     private int size;
 
     public LinkedList() {
-        head = null;
+        dummyHead = new Node();
         size = 0;
     }
 
@@ -46,11 +46,12 @@ public class LinkedList<E> {
 
     // 在链表头添加新的元素e
     public void addFirst(E e) {
-//        Node node = new Node(e);
-//        node.next = head;
-//        head = node;
-        head = new Node(e, head);
-        size++;
+        add(0, e);
+    }
+
+    // 在链表末尾添加新的元素e
+    public void addLast(E e) {
+        add(size, e);
     }
 
     // 在链表的 index(0-based) 位置添加新的元素e
@@ -58,23 +59,13 @@ public class LinkedList<E> {
         if (index < 0 || index > size) {
             throw new IllegalArgumentException("Add failed. Illegal index.");
         }
-        if (index == 0) {
-            addFirst(e);
-        } else {
-            Node prev = head;
-            for (int i = 0; i < index - 1; i++) {
-                prev = prev.next;
-            }
-//            Node node = new Node(e);
-//            node.next = prev.next;
-//            prev.next = node;
-            prev.next = new Node(e, prev.next);
-            size++;
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
         }
+        prev.next = new Node(e, prev.next);
+        size++;
     }
 
-    // 在链表末尾添加新的元素e
-    public void addLast(E e) {
-        add(size, e);
-    }
+
 }
