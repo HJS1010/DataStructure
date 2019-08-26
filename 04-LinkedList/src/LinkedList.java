37ba6592cc48ae8b50ca6bd97a965b0ede226724
ltjs1024 <78282271@qq.com>
2019-08-26 13:52:19 +0800

    LinkedList init

---
+++ 04-LinkedList/src/LinkedList.java
@@ -0,0 +1,45 @@
+/**
+ * 链表
+ */
+public class LinkedList<E> {
+
+    private class Node {
+        private E e;
+        private Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+
+    private Node head;
+    private int size;
+
+    public LinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+}
