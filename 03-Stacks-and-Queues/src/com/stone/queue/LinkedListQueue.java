a04df032f68a5bb46ff91d00ceaa7fdf261e258d
ltjs1024 <78282271@qq.com>
2019-08-27 10:57:59 +0800

    refactor LinkedListQueue

--- 03-Stacks-and-Queues/src/com/stone/queue/LinkedListQueue.java
+++ 03-Stacks-and-Queues/src/com/stone/queue/LinkedListQueue.java
@@ -4,47 +4,101 @@ import com.stone.LinkedList;
 
 /**
  * 基于链表实现的队列
- * enqueue: O(n)
+ * enqueue: O(1)
  * dequeue: O(1)
  * getFront: O(1)
  */
 public class LinkedListQueue<E> implements Queue<E> {
-    private LinkedList<E> list;
+    private class Node {
+        private E e;
+        private Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
 
     public LinkedListQueue() {
-        list = new LinkedList<>();
+        head = null;
+        tail = null;
+        size = 0;
     }
 
     @Override
     public void enqueue(E e) {
-        list.addLast(e);
+        if (tail == null) {
+            tail = new Node(e);
+            head = tail;
+        } else {
+            tail.next = new Node(e);
+            tail = tail.next;
+        }
+        size++;
     }
 
     @Override
     public E dequeue() {
-        return list.removeFirst();
+        if (isEmpty()) {
+            throw new IllegalArgumentException(
+                    "Can't dequeue from an empty queue.");
+        }
+        Node retNode = head;
+        head = retNode.next;
+        retNode.next = null;
+        if (head == null) {
+            tail = null;
+        }
+        size--;
+        return retNode.e;
     }
 
     @Override
     public E getFront() {
-        return list.getFirst();
+        if (isEmpty()) {
+            throw new IllegalArgumentException("Queue is empty.");
+        }
+        return head.e;
     }
 
     @Override
     public int getSize() {
-        return list.getSize();
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return list.isEmpty();
+        return size == 0;
     }
 
     @Override
     public String toString() {
         StringBuilder res = new StringBuilder();
         res.append("Queue: front ");
-        res.append(list);
+        Node cur = head;
+        while (cur != null) {
+            res.append(cur.e);
+            res.append("->");
+            cur = cur.next;
+        }
+        res.append("NULL tail");
         return res.toString();
     }
 
