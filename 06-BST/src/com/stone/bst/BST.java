f88935f93267b1d4f8adaa98d73f5d5dc31c37ec
ltjs1024 <78282271@qq.com>
2019-08-30 09:48:34 +0800

    refactor add element

--- 06-BST/src/com/stone/bst/BST.java
+++ 06-BST/src/com/stone/bst/BST.java
@@ -1,5 +1,8 @@
 package com.stone.bst;
 
+/**
+ * Binary Search Tree：二分搜索树
+ */
 public class BST<E extends Comparable<E>> {
 
     private class Node {
@@ -37,30 +40,21 @@ public class BST<E extends Comparable<E>> {
 
     // 向二分搜索树中添加新的元素e
     public void add(E e) {
-        if (root == null) {
-            root = new Node(e);
-            size++;
-        } else {
-            add(root, e);
-        }
+        root = add(root, e);
     }
 
     // 向以node为根的二分搜索树中插入元素e，递归算法
-    private void add(Node node, E e) {
-        if (e.equals(node.e)) {
-            return;
-        } else if (e.compareTo(node.e) < 0 && node.left == null) {
-            node.left = new Node(e);
+    // 返回插入新节点后的二分搜索树的根
+    private Node add(Node node, E e) {
+        if (node == null) {
             size++;
-        } else if (e.compareTo(node.e) > 0 && node.right == null) {
-            node.right = new Node(e);
-            size++;
+            return new Node(e);
         }
-
         if (e.compareTo(node.e) < 0) {
-            add(node.left, e);
+            node.left = add(node.left, e);
         } else if (e.compareTo(node.e) > 0) {
-            add(node.right, e);
+            node.right = add(node.right, e);
         }
+        return node;
     }
 }
