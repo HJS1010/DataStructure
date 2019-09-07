8cb7fdb52d0658cf4f17de5f39af25075f0ba4bd
Squirrel913 <78282271@qq.com>
2019-09-07 15:11:34 +0800

    topKFrequent

---
+++ 08-Heap-and-Priority-Queue/src/com/stone/queue/Solution347.java
@@ -0,0 +1,83 @@
+package com.stone.queue;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.TreeMap;
+
+/**
+ * 给定一个非空的整数数组，返回其中出现频率前 k 高的元素。
+ * <p>
+ * 示例 1:
+ * 输入: nums = [1,1,1,2,2,3], k = 2
+ * 输出: [1,2]
+ * <p>
+ * 示例 2:
+ * 输入: nums = [1], k = 1
+ * 输出: [1]
+ * <p>
+ * 链接：https://leetcode-cn.com/problems/top-k-frequent-elements
+ */
+public class Solution347 {
+
+    private class Freq implements Comparable<Freq> {
+        private int e;
+        private int freq;
+
+        public Freq(int e, int freq) {
+            this.e = e;
+            this.freq = freq;
+        }
+
+        @Override
+        public int compareTo(Freq another) {
+            if (this.freq < another.freq) {
+                return 1;
+            } else if (this.freq > another.freq) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+
+        PriorityQueue<Freq> pq = new PriorityQueue<>();
+        for (int key : map.keySet()) {
+            if (pq.getSize() < k) {
+                pq.enqueue(new Freq(key, map.get(key)));
+            } else if (pq.getFront().freq < map.get(key)) {
+                pq.dequeue();
+                pq.enqueue(new Freq(key, map.get(key)));
+            }
+        }
+        LinkedList<Integer> list = new LinkedList<>();
+        while (!pq.isEmpty()) {
+            list.add(pq.dequeue().e);
+        }
+
+        return list;
+    }
+
+    private static void printList(List<Integer> nums) {
+        for (Integer num : nums)
+            System.out.print(num + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 1, 1, 2, 2, 3};
+        int k = 2;
+        System.out.println(((new Solution347()).topKFrequent(nums, k)));
+    }
+
+}
