c22cd722a78e00f00198691ada6f4348f17e98fd
Squirrel913 <78282271@qq.com>
2019-09-12 14:42:39 +0800

    query in Segment

--- 09-Segment-Tree/src/com/stone/seg/Main.java
+++ 09-Segment-Tree/src/com/stone/seg/Main.java
@@ -7,8 +7,13 @@ public class Main {
         Integer[] nums = {-2, 0, 3, -5, 2, -1};
 
         SegmentTree<Integer> segTree = new SegmentTree<>(
-                nums, (a, b) -> a + b);
+                nums, Integer::sum);
 
         System.out.println(segTree);
+
+
+        System.out.println(segTree.query(0, 2));
+        System.out.println(segTree.query(2, 5));
+        System.out.println(segTree.query(0, 5));
     }
 }
--- 09-Segment-Tree/src/com/stone/seg/SegmentTree.java
+++ 09-Segment-Tree/src/com/stone/seg/SegmentTree.java
@@ -15,7 +15,7 @@ public class SegmentTree<E> {
             data[i] = arr[i];
         }
         tree = (E[]) new Object[4 * arr.length];
-        buildSegmentTree(0, 0, data.length - 1);
+        buildSegmentTree(0, 0, arr.length - 1);
     }
 
     // 在treeIndex的位置创建表示区间[lt...rt]的线段树
@@ -44,12 +44,43 @@ public class SegmentTree<E> {
         return data[index];
     }
 
+    // 返回完全二叉树的数组表示中，一个索引所表示的元素的左孩子节点的索引
     private int leftChild(int index) {
         return 2 * index + 1;
     }
 
+    // 返回完全二叉树的数组表示中，一个索引所表示的元素的右孩子节点的索引
     private int rightChild(int index) {
         return 2 * index + 2;
+    }
+
+    // 返回区间[queryL, queryR]的值
+    public E query(int queryL, int queryR) {
+        if (queryL < 0 || queryL >= data.length ||
+                queryR < 0 || queryR >= data.length || queryL > queryR) {
+            throw new IllegalArgumentException("Illegal Index.");
+        }
+        return query(0, 0, data.length - 1, queryL, queryR);
+    }
+
+    // 在以treeIndex为根的线段树中[lt...rt]的范围里，搜索区间[queryL...queryR]的值
+    private E query(int treeIndex, int lt, int rt, int queryL, int queryR) {
+        if (lt == queryL && rt == queryR) {
+            return tree[treeIndex];
+        }
+        int mid = lt + (rt - lt) / 2;
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+        if (queryL >= mid + 1) {
+            return query(rightTreeIndex, mid + 1, rt, queryL, queryR);
+        } else if (queryR <= mid) {
+            return query(leftTreeIndex, lt, mid, queryL, queryR);
+        }
+
+        E leftResult = query(leftTreeIndex, lt, mid, queryL, mid);
+        E rightResult = query(rightTreeIndex, mid + 1, rt, mid + 1, queryR);
+
+        return merger.merge(leftResult, rightResult);
     }
 
     @Override
